6be94814057d6dcff2307443efd912f24dbf5de1
taka <neithi.gensin@gmail.com>
2023-07-04 22:10:25 +0900

    first_commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export const App = () => {
+    return (
+        <div>
+            <h1>請求書アプリ</h1>
+            <p>フロント</p>
+            <a href="https://www.yahoo.co.jp/">YAHOOへ遷移</a>
+        </div>
+    );
+};
+
