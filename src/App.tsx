aeffe6f8bd0d5be49b1eda3749c39c907142abf0
taka <neithi.gensin@gmail.com>
2023-07-04 22:13:53 +0900

    React+Webpackを使用した環境構築,バックエンドとのreq,res受け渡しOK

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import * as React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    // User型のインターフェースを定義する
+    interface User {
+        id: number;
+        mail: string;
+    }
+
+    const handleButtonClick = () => {
+        fetch("http://localhost:3000/api/users") // バックエンドのポート番号を指定
+        .then(response => response.json())
+            .then(data => setUsers(data))
+            .catch(error => console.error(error));
+    };
+
     return (
         <div>
             <h1>請求書アプリ</h1>
             <p>フロント</p>
-            <a href="https://www.yahoo.co.jp/">YAHOOへ遷移</a>
+            <a href="https://www.yahoo.co.jp/">YAHOOへ遷移</a><br />
+
+            <button onClick={handleButtonClick}>ユーザー情報を取得</button>
+            
+            {users.length > 0 ? (
+                <ul>
+                    {users.map(user => (
+                        <li key={user.id}>{user.mail}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>ユーザー情報がありません。</p>
+            )}
         </div>
     );
 };
-
