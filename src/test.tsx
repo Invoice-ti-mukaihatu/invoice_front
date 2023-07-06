c8a0e07ab79ed7be06a6c09c5748dc81b3b37de5
taka <neithi.gensin@gmail.com>
2023-07-06 21:47:23 +0900

    シングルページ遷移完了

---
+++ src/test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const TestPage = () => {
+    return (
+        <div>
+            <h1>別ファイル画面</h1>
+            <p>これは別のファイルに遷移した画面です。</p>
+        </div>
+    );
+};
