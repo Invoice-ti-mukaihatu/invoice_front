1d2cb75936b465062bc54c4dc2645bbce5178c54
taka <neithi.gensin@gmail.com>
2023-08-11 00:15:39 +0900

    ファイル階層編集

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login } from "./pages/Login";
-import { Main } from "./pages/Main";
+import { Login } from "./pages/login/Login";
+import { Main } from "./pages/main/Main";
 import axios from "axios";
 
 export const App = () => {
