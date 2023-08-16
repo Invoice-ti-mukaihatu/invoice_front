766ea2bbf747ee13598be875a806e51f1bc55a42
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-08-16 22:44:06 +0900

    config→envに変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/login/Login";
 import { Main } from "./pages/main/Main";
 import axios from "axios";
-import { BACKEND_BASE_URL } from "./utilis/config";
+import { BACKEND_BASE_URL } from "./env";
 
 export const App = () => {
   axios.defaults.withCredentials = true;
---
+++ src/env.ts
@@ -0,0 +1,3 @@
+// envファイルの代わり
+// ここに追加した場合にはGithubActionsにも追加する必要がある
+export const BACKEND_BASE_URL = "http://localhost:3000";
