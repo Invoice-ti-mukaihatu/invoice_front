ae5334670a965713461b142311c66329474be316
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-09-17 16:27:22 +0900

    ユーザー編集とパスワード編集画面の作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Login } from "./pages/login/Login";
 import axios from "axios";
 import { BACKEND_BASE_URL } from "./env";
 import { Menu } from "./pages/menu/Menu";
+import { UserEdit } from "./pages/userEdit/UserEdit";
+import { PasswordEdit } from "./pages/passwordEdit/PasswordEdit";
 
 export const App = () => {
   axios.defaults.withCredentials = true;
@@ -14,6 +16,8 @@ export const App = () => {
       <Routes>
         <Route path="/" Component={Login} />
         <Route path="/menu" Component={Menu} />
+        <Route path="/user_edit" Component={UserEdit} />
+        <Route path="/password_edit" Component={PasswordEdit} />
       </Routes>
     </BrowserRouter>
   );
